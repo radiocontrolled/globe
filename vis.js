edbb42aa0d264fa99bd10ed1f4d2bb24fcbc0ef4
radiocontrolled <radiocontrolled@gmail.com>
2014-10-01 17:35:47 +0100

    the globe is properly responsive.

--- vis.js
+++ vis.js
@@ -1,53 +1,60 @@
-var height = $(window).height() * 0.9999,
-	width = $(window).width()* 0.9999,
+var height = $(window).height() / .9999,
+	width = $(window).width() / .9999,
 	manualRotationActivated = false,
-	sens = 0.25,
-	globe;
+	k = 0.25,
+	globe,
+	projection,
+	path;
 
-
-var λ = d3.scale.linear()
-    .domain([0, width])
-    .range([-180, 180]);
-
-var φ = d3.scale.linear()
-    .domain([0, height])
-    .range([90, -90]);
-
-globe = d3.select("section")
-	.append("svg")
-	.attr({
-		"width": width,
-		"height": height
-	});
 	
-
-
-
-// define map projection
-var projection = d3.geo.orthographic()
-	.scale(width/5)
-	.translate([width/2, height/2]) // center the projection
-	.rotate([0, -15, 0])
-	.clipAngle(90);
-
-var path = d3.geo.path()
-	.projection(projection);
+var setup = function(w,h) {
+	globe = d3.select("section")
+		.append("svg")
+		.attr("id", "globe")
+		.style("width", w + 'px')
+		.style("height", h + 'px')
+		
+		
+	// define map projection
+	projection = d3.geo.orthographic()
+		.scale(w/5)
+		.translate([w/2, h/2]) // center the projection
+		.rotate([0, -15, 0])
+		.clipAngle(90);
+	
+	
+	path = d3.geo.path()
+		.projection(projection);
   
-/* create a globe */
-globe.append("path")
-	.datum({type: "Sphere"})
-	.attr("class", "water")
-	.attr("d", path);
+	/* create a globe */
+	globe.append("path")
+		.datum({type: "Sphere"})
+		.attr("class", "water")
+		.attr("d", path);
 	
+}
+
+setup(width,height);
 
 
 /* load and display data */
 queue()
 	.defer(d3.json, "countries.json") /* topjson */
+	.defer(d3.csv, "nominees.csv") /* nominees*/
 	.await(countries);
   
-function countries(error, worldTopo){
+function countries(error, worldTopo, nominees){
 
+	/* populate nominee list */
+	/*
+	var legend = d3.select("#legend");
+	for(var name in nominees){
+		legend
+			.append("li")
+			.text(function(){
+				return nominees[name]["nominee"];
+			})
+	}*/
 	
 	var countriesData = topojson.feature(worldTopo, worldTopo.objects.countries).features;
 	
@@ -59,10 +66,10 @@ function countries(error, worldTopo){
 
 	world
 		.call(d3.behavior.drag()
-		.origin(function() { var r = projection.rotate(); return {x: r[0] / sens, y: -r[1] / sens}; })
+		.origin(function() { var r = projection.rotate(); return {x: r[0] / k, y: -r[1] / k}; })
 		.on("drag", function() {
 			var rotate = projection.rotate();
-			projection.rotate([d3.event.x * sens, -d3.event.y * sens, rotate[2]]);
+			projection.rotate([d3.event.x * k, -d3.event.y * k, rotate[2]]);
 			globe.selectAll("path").attr("d", path);
 		  }));
 }
@@ -71,6 +78,26 @@ function countries(error, worldTopo){
 d3.select(window).on('resize', resize);
 
 function resize(globe) {
+	height = $(window).height() * 0.9999;
+	width = $(window).width()* 0.9999;
 	
+	projection
+		.scale(width/5)
+		.translate([width/2, height/2]) // center the projection
+	
+	path = d3.geo.path()
+		.projection(projection);
+	
+	
+	// resize the SVG containing the globe
+	d3.select("#globe")
+		.style("height", height + "px")
+		.style("width", width + "px");
+	
+
+	d3.select(".water").attr('d', path);
+	d3.selectAll(".country").attr('d', path);
+
+
 	
 }
